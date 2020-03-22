da4784126e3bc23e56e4c083899c076eae9a297c
aoi apache <aoi.182@live.com>
2020-03-22 10:44:02 -0500

    fisrt commit

---
+++ MySolution/IdentityExample/Controllers/HomeController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NETCore.MailKit.Core;
+using System.Threading.Tasks;
+
+namespace IdentityExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IEmailService _emailService;
+        public HomeController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IEmailService emailService)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _emailService = emailService;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+            if (user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, password, false, false);
+
+                if (result.Succeeded)
+                    RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [Authorize]
+        public ActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(string username, string email, string password)
+        {
+            var user = new IdentityUser(username) { Email = email };
+
+            var result = await _userManager.CreateAsync(user, password);
+
+            if (result.Succeeded)
+            {
+                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+                var link = Url.Action(nameof(VerifyEmail), "Home", new { userId = user.Id, code = code }, Request.Scheme, Request.Host.ToString());
+
+                //await _emailService.SendAsync(user.Email, "Email verify", $@"<a href=\{link}></a>", true);
+
+                return RedirectToAction(nameof(VerifyEmail), new { userId = user.Id, code = code });
+
+                // todo fix email send and then use the view EmailVerification
+                //return RedirectToAction(nameof(EmailVerification)); 
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> VerifyEmail(string userId, string code)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user == null) return BadRequest();
+
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+
+            if (!result.Succeeded) return BadRequest();
+
+            return View();
+        }
+
+        public IActionResult EmailVerification() => View();
+
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MySolution/MyWebApp/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace MyWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Secret()
+        {
+            return View();
+        }
+
+        public ActionResult Authenticate()
+        {
+            var grandmaClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "Bob"),
+                new Claim(ClaimTypes.Email, "Bob@fmail.co"),
+                new Claim("Grandma.Says", "Vwery nice boy."),
+            };
+
+            var licenseClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "Bob-182"),
+                new Claim("DrivingLicense", "A8457+?++2."),
+            };
+
+            var grandmaIdentity = new ClaimsIdentity(grandmaClaims, "Grandma Identity");
+            var licenseIdentity = new ClaimsIdentity(licenseClaims, "Government");
+
+
+            var userPrincipal = new ClaimsPrincipal(new[] { grandmaIdentity, licenseIdentity });
+
+            HttpContext.SignInAsync(userPrincipal);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
